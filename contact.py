f564451474a0c3c9947b3021b227f4a19afcd78d
DanielSBrown <dsb9ef@virginia.edu>
2019-10-27 11:20:42 -0400

    Initial build of /contact for get, post, and delete

---
+++ contact.py
@@ -0,0 +1,43 @@
+class Contact:
+
+    def __init__(self, fName, lName):
+        self.fName = fName
+        self.lName = lName
+
+def searchClusterForContact(contact, es, size):
+    # Queries the ES cluster, and returns if there are any hits
+    if contact.fName is None or contact.lName is None:
+        return None
+    doc = {
+        "query": {
+            "bool": {
+                "must": [
+                    {
+                    "term": {
+                        "fName": contact.fName
+                    }},
+                    {
+                    "term": {
+                        "lName": contact.lName
+                    }
+                    }
+                ]
+            }
+        },
+        "size": size
+
+    }
+    return es.search(index='contacts', body=doc)
+
+def doesContactExist(contact, es):
+    response = searchClusterForContact(contact, es, 1)
+    return response["hits"]["total"] > 0
+
+def getNamesFromName(name):
+    try:
+        names = name.split()
+        if len(names) < 2:
+            return None, None
+        return name.split()[0], name.split()[1]
+    except:
+        return None, None
---
+++ main.py
@@ -0,0 +1,62 @@
+from datetime import datetime
+from flask import Flask, jsonify, request, abort
+from elasticsearch import Elasticsearch
+import json
+from contact import *
+es = Elasticsearch()
+
+app = Flask(__name__)
+@app.route('/contact', methods=['GET'])
+def getAllContacts():
+    doc = {
+        'size' : 10000,
+        'query': {
+            'match_all' : {}
+       }
+   }
+    result = es.search(index='contacts', body=doc)
+    return jsonify(result)
+
+
+@app.route('/contact', methods=['POST'])
+def createContact():
+    # Begin by formatting the data sent in the request into a JSON object
+
+    my_json = (request.data.decode('utf8').replace("'", '"'))
+    d = json.loads(my_json)
+
+    # Get the fields from the body
+
+    fName = d["fName"]
+    lName = d["lName"]
+    contact = Contact(fName, lName)
+
+    # Check for existing contact
+    if doesContactExist(contact, es):
+        abort(400, "Contact already exists")
+
+    body = {
+        'fName': fName,
+        'lName': lName,
+        'timestamp': datetime.now()
+    }
+
+    result = es.index(index='contacts', doc_type='contact',  body=body)
+
+    return jsonify(result)
+
+
+
+# @app.route('contact', methods=['PUT'])
+# def updateContact:
+#
+@app.route('/contact/<name>', methods=['DELETE'])
+def deleteContact(name):
+    fName, lName = getNamesFromName(name)
+    contact = Contact(fName, lName)
+    response = searchClusterForContact(contact, es, 1)
+    if response["hits"]["total"] == 0:
+        abort(400, "User does not exist")
+    result = es.delete(index='contacts', doc_type='contact', id=response["hits"]["hits"][0]["_id"])
+
+    return jsonify(result)
